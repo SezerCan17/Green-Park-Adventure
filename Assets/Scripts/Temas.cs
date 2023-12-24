15dc5063f0a4c3f299aa15cf4c3b56145a97440b
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2023-12-25 00:16:28 +0300

    toplama

--- Assets/Scripts/Temas.cs
+++ Assets/Scripts/Temas.cs
@@ -4,11 +4,57 @@ using UnityEngine;
 
 public class Temas : MonoBehaviour
 {
+    private bool icerde;
+    public Animator animator;
+    private GameObject temasEdilenObj;
+
+    private void Awake()
+    {
+        
+        icerde = false;
+    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if(collision.gameObject.tag=="Plastic")
+    //    {
+
+    //        collision.gameObject.SetActive(false);
+    //    }
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.tag=="Plastic")
         {
-            Debug.Log("qwer");
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde =true;
+            
+                //other.gameObject.SetActive(false);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag=="Plastic")
+        {
+            Debug.Log("Dýsarda");
+            icerde =false;
+            
+        }
+    }
+
+    private void Update()
+    {
+        if(icerde && Input.GetKeyDown(KeyCode.E))
+        {
+            SilObjeyi();
+        }
+    }
+
+    void SilObjeyi()
+    {
+        temasEdilenObj.SetActive(false);
+        animator.SetTrigger("collect");
     }
 }
