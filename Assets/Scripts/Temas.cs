e4274d589ec6bb828ab79af52585e4c618be6c01
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2023-12-28 00:48:14 +0300

    kağıt, metal

--- Assets/Scripts/Temas.cs
+++ Assets/Scripts/Temas.cs
@@ -7,20 +7,15 @@ public class Temas : MonoBehaviour
     private bool icerde;
     public Animator animator;
     private GameObject temasEdilenObj;
+    public UIManager uiManager;
+    private bool eldevar;
 
     private void Awake()
     {
         
         icerde = false;
     }
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if(collision.gameObject.tag=="Plastic")
-    //    {
-
-    //        collision.gameObject.SetActive(false);
-    //    }
-    //}
+   
 
     private void OnTriggerEnter(Collider other)
     {
@@ -30,7 +25,39 @@ public class Temas : MonoBehaviour
             Debug.Log("icerde");
             icerde =true;
             
-                //other.gameObject.SetActive(false);
+        }
+        if(other.gameObject.tag=="PlasticBin")
+        {
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde = true;
+        }
+
+        if (other.gameObject.tag == "metal")
+        {
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde = true;
+
+        }
+        if (other.gameObject.tag == "MetalBin")
+        {
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde = true;
+        }
+        if (other.gameObject.tag == "kagit")
+        {
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde = true;
+
+        }
+        if (other.gameObject.tag == "PaperBin")
+        {
+            temasEdilenObj = other.gameObject;
+            Debug.Log("icerde");
+            icerde = true;
         }
     }
 
@@ -42,19 +69,119 @@ public class Temas : MonoBehaviour
             icerde =false;
             
         }
+        if(other.gameObject.tag=="PlasticBin")
+        {
+            icerde=false;
+        }
+        if (other.gameObject.tag == "metal")
+        {
+            Debug.Log("Dýsarda");
+            icerde = false;
+
+        }
+        if (other.gameObject.tag == "MetalBin")
+        {
+            icerde = false;
+        }
+        if (other.gameObject.tag == "kagit")
+        {
+            Debug.Log("Dýsarda");
+            icerde = false;
+
+        }
+        if (other.gameObject.tag == "PaperBin")
+        {
+            icerde = false;
+        }
     }
 
     private void Update()
     {
         if(icerde && Input.GetKeyDown(KeyCode.E))
         {
-            SilObjeyi();
+            if(temasEdilenObj.tag=="Plastic")
+            {
+                SilObjeyi();
+            }
+            if(temasEdilenObj.tag=="PlasticBin")
+            {
+                Plastic_yok();
+            }
+            if (temasEdilenObj.tag == "metal")
+            {
+                SilObjeyi2();
+            }
+            if (temasEdilenObj.tag == "MetalBin")
+            {
+                metal_yok();
+            }
+            if (temasEdilenObj.tag == "kagit")
+            {
+                SilObjeyi3();
+            }
+            if (temasEdilenObj.tag == "PaperBin")
+            {
+                kagit_yok();
+            }
+
         }
     }
 
     void SilObjeyi()
     {
+        Plastic_var();
         temasEdilenObj.SetActive(false);
         animator.SetTrigger("collect");
+    }
+
+    public void Plastic_var()
+    {
+        uiManager.plastic_var();
+        eldevar = true;
+    }
+
+    public void Plastic_yok()
+    {
+        uiManager.plastic_var2();
+        eldevar=false;
+    }
+
+
+    void SilObjeyi2()
+    {
+        metal_var();
+        temasEdilenObj.SetActive(false);
+        animator.SetTrigger("collect");
+    }
+
+    public void metal_var()
+    {
+        uiManager.metal_var();
+        eldevar = true;
+    }
+
+    public void metal_yok()
+    {
+        uiManager.metal_var2();
+        eldevar = false;
+    }
+
+    void SilObjeyi3()
+    {
+        kagit_var();
+        temasEdilenObj.SetActive(false);
+        animator.SetTrigger("collect");
+    }
+
+    public void kagit_var()
+    {
+        uiManager.kagit_var();
+        eldevar = true;
+    }
+
+    public void kagit_yok()
+    {
+        uiManager.kagit_var2();
+        eldevar = false;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,16 +7,66 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     private bool eldevar;
-    public Image image;
+    public GameObject image;
+    public GameObject image2;
+    public GameObject image3;
+    public GameObject plastic_yesil;
+    public GameObject metal_sarý;
+    public GameObject kagit_mavi;
     private void Awake()
     {
         eldevar = false;
     }
 
-    public void Elde_var()
+    public void plastic_var()
     {
+        image.SetActive(true);
+    }
+
+    public void plastic_var2()
+    {
+        image.SetActive(false);
+        plastic_yesil.SetActive(true);
+        StartCoroutine(MyEnumerator());
+        
+    }
+
+    public void metal_var()
+    {
+        image2.SetActive(true);
+    }
+
+    public void metal_var2()
+    {
+        image2.SetActive(false);
+        metal_sarý.SetActive(true);
+        StartCoroutine(MyEnumerator());
 
     }
 
-    
+    public void kagit_var()
+    {
+        image3.SetActive(true);
+    }
+
+    public void kagit_var2()
+    {
+        image3.SetActive(false);
+        kagit_mavi.SetActive(true);
+        StartCoroutine(MyEnumerator());
+
+    }
+
+    IEnumerator MyEnumerator()
+    {
+        Debug.Log("Enumerator baþladý.");
+
+        yield return new WaitForSeconds(4f); 
+        Debug.Log("2 saniye bekledik.");
+        plastic_yesil.SetActive(false);
+        metal_sarý.SetActive(false);
+        kagit_mavi.SetActive(false);
+    }
+
+
 }
